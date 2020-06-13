89f2d6b709a1f441686be43e4ca51fdf4c3b8fbe
Garvit291 <chhabragarvit291@outlook.com>
2020-06-13 18:11:05 +0530

    Updates

---
+++ precache-manifest.5cffc8ef696ee4700f7957e80b37edff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dab8f6b84540fed607e6d56412d4769",
+    "url": "/Covid19-tracker/index.html"
+  },
+  {
+    "revision": "2e098c955a9d1e1fe431",
+    "url": "/Covid19-tracker/static/css/main.7ae8d217.chunk.css"
+  },
+  {
+    "revision": "cac16a15fa572cca6cdc",
+    "url": "/Covid19-tracker/static/js/2.106d51d1.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid19-tracker/static/js/2.106d51d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e098c955a9d1e1fe431",
+    "url": "/Covid19-tracker/static/js/main.12ce0ce2.chunk.js"
+  },
+  {
+    "revision": "f8de5687ca5cdbf6de4e",
+    "url": "/Covid19-tracker/static/js/runtime-main.c24b01d9.js"
+  },
+  {
+    "revision": "8bc12995e959d4e852a237ffe65d70ca",
+    "url": "/Covid19-tracker/static/media/covid.8bc12995.jpg"
+  }
+]);
